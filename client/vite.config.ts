c76f65fe25c9cd597a4de85b4989b0e6b10c1797
Dimitar Krastanov <dimitarkrustanov@gmail.com>
2025-02-25 23:12:27 +0200

    fixed dependancies erros

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api/v1/analytics_events': {
+        target: 'https://auth.privy.io',
+        changeOrigin: true,
+        secure: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      }
+    }
+  }
 })
